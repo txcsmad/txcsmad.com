7e02d4145e8c349ef3c1bdaed02c9509bb2d856c
Drew Romanyk <drewiswaycool@gmail.com>
2016-08-16 08:08:04 -0500

    Add rest app to the common settings

--- config/settings/common.py
+++ config/settings/common.py
@@ -55,6 +55,7 @@ THIRD_PARTY_APPS = (
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'rest_framework', # Rest API
 )
 
 # Apps specific for this project go here.
