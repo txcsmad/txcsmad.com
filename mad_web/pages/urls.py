8a384e7e80a31a4b62f3eccc61c3123ccb560b7a
Nicholas Walker <nick@nickwalker.us>
2017-02-27 20:22:33 -0600

    Remove views import Causing issues with deployment

--- mad_web/pages/urls.py
+++ mad_web/pages/urls.py
@@ -4,10 +4,10 @@ from __future__ import absolute_import, unicode_literals
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from . import views
-
 urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
     url(r'^workshops/$', TemplateView.as_view(template_name='pages/workshops.html'), name='workshops'),
-    url(r'^labs/$', TemplateView.as_view(template_name='pages/labs.html'), {'description': 'MAD Labs is a subset of the most technically experienced and passionate MAD members that focuses on creating quality mobile apps for others. Members specialize in iOS, Android, Windows or web platforms.'}, name='labs'),
+    url(r'^labs/$', TemplateView.as_view(template_name='pages/labs.html'), {
+        'description': 'MAD Labs is a subset of the most technically experienced and passionate MAD members that focuses on creating quality mobile apps for others. Members specialize in iOS, Android, Windows or web platforms.'},
+        name='labs'),
 ]
