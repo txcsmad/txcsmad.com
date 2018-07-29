3c7e80e6e079334326e872e2404854aeba23d778
Ali Naqvi <alinaqvi1397@gmail.com>
2018-07-29 00:20:15 -0500

    Reverted to fix errors, will try new approach without breaking site

--- mad_web/pages/urls.py
+++ mad_web/pages/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
     url(r'^workshops/$', TemplateView.as_view(template_name='pages/workshops.html'), name='workshops'),
-    # url(r'^labs/$', TemplateView.as_view(template_name='pages/labs.html'), {
-    #     'description': 'MAD Labs is a subset of the most technically experienced and passionate MAD members that focuses on creating quality mobile apps for others. Members specialize in iOS, Android, Windows or web platforms.'},
-    #     name='labs'),
+    url(r'^labs/$', TemplateView.as_view(template_name='pages/labs.html'), {
+        'description': 'MAD Labs is a subset of the most technically experienced and passionate MAD members that focuses on creating quality mobile apps for others. Members specialize in iOS, Android, Windows or web platforms.'},
+        name='labs'),
 ]
